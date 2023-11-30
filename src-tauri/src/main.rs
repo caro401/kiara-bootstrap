a4848e0c59b84b7ef26a7c64d17e446488d33a69
Caro Appleby <caroline.appleby@cantab.net>
2023-11-30 15:05:09 +0000

    Make it kinda work

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE -- tauri!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use duct::cmd;
 use sha2::{Digest, Sha256};
 use std::collections::HashMap;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::{fs, io};
+use std::fmt::format;
+use std::time::Duration;
 use tauri::api::process::{Command, CommandEvent};
+use tauri::{App, AppHandle, Manager};
+
+// hard code this python version for now
+// if you change this, there's changes needed in pixi.toml
+// and the definition file (get this from https://github.com/pyenv/pyenv/tree/master/plugins/python-build/share/python-build)
+static PYTHON_VERSION: &str = "3.11.5";
 
 fn start_main_app(env_vars: HashMap<String, String>) {
     let (mut rx, mut child) = Command::new_sidecar("kiara-tauri")
@@ -20,102 +29,136 @@ fn start_main_app(env_vars: HashMap<String, String>) {
             if let CommandEvent::Stdout(line) = &event {
                 println!("{line}")
             }
-             if let CommandEvent::Stderr(line) = &event {
+            if let CommandEvent::Stderr(line) = &event {
                 eprintln!("{line}")
             }
         }
     });
 }
 
-fn hash_file_at_path(filepath: &Path) -> io::Result<String> {
-    let mut hasher = Sha256::new();
-    let mut file_to_hash = fs::File::open(filepath).unwrap();
-    io::copy(&mut file_to_hash, &mut hasher).unwrap();
-    Ok(base16::encode_lower(&hasher.finalize()))
+fn log_to_frontend(handle: &AppHandle, message: impl Into<String>) {
+    handle
+        .emit_all(
+            "logevent",
+            Message {
+                message: message.into(),
+            },
+        )
+        .unwrap();
+}
+
+fn right_python_exists(kiara_appconfig_dir: &Path) -> bool {
+    let python_command = kiara_appconfig_dir.to_owned().join("python/bin/python");
+    let python_version_output = cmd!(python_command, "--version").stdout_capture().read();
+    match dbg!(python_version_output) {
+        Ok(output) => {
+            dbg!(output.trim()) == format!("Python {PYTHON_VERSION}")
+        }
+        Err(_) => false,
+    }
+}
+
+fn get_resource_path(handle: &AppHandle, pathname: &str) -> PathBuf {
+    handle
+        .path_resolver()
+        .resolve_resource(format!("resources/{pathname}"))
+        .expect(&format!("failed to find {pathname} file in app resources"))
+}
+fn right_requirements_exist(handle: &AppHandle,kiara_appconfig_dir: &Path ) -> bool {
+    let existing_requirements_path = kiara_appconfig_dir.join("requirements.txt");
+    let existing_requirements = std::fs::read_to_string(&existing_requirements_path);
+    let requirements_resource = get_resource_path(&handle, "requirements.txt");
+    let new_requierements =  std::fs::read_to_string(&requirements_resource).unwrap();
+    existing_requirements.ok().as_ref() ==  Some(&new_requierements)
+}
+
+fn compile_python(kiara_appconfig_dir: &Path) {
+        std::fs::create_dir_all(&kiara_appconfig_dir).unwrap();
+        let install_result = std::process::Command::new("pixi")
+        .arg("install")
+        .current_dir(&kiara_appconfig_dir)
+        .spawn()
+        .expect("Failed to pixi install. Do you have pixi on the system?")
+        .wait()
+        .expect("Something went wrong with pixi");
+    assert!(
+        install_result.success(),
+        "Failed to set up environment with pixi"
+    );
+
+    let python_result = std::process::Command::new("pixi")
+        .arg("run")
+        .arg("compile-python")
+        .current_dir(&kiara_appconfig_dir)
+        .spawn()
+        .expect("Failed to run pixi command. Do you have pixi on the system.unwrap()")
+        .wait()
+        .expect("Something went wrong with pixi.unwrap()");
+    assert!(python_result.success(), "Failed to compile python");
+
+}
+
+fn pip_install(kiara_appconfig_dir: &Path) {
+    let python_deps_result = std::process::Command::new("./python/bin/python")
+        .args(["-m", "pip", "install", "-r", "requirements.txt"])
+        .current_dir(&kiara_appconfig_dir)
+        .spawn()
+        .expect("Failed to run pip. Is your python install ok?")
+        .wait()
+        .expect("Something went wrong with installing packages");
+    assert!(python_deps_result.success(), "Failed to install kiara");
+}
+
+fn copy_resource_file(handle: &AppHandle, kiara_appconfig_dir: &Path, filename: &str) {
+    let resource =  get_resource_path(&handle, filename);
+    std::fs::copy(resource, kiara_appconfig_dir.join(filename)).unwrap();
+}
+
+fn copy_resources(handle: &AppHandle,kiara_appconfig_dir: &Path ) {
+    std::fs::create_dir_all(&kiara_appconfig_dir).unwrap();
+    let files_to_copy = ["pixi.lock", "pixi.toml", PYTHON_VERSION, "requirements.txt"];
+    for file in files_to_copy {
+        copy_resource_file(&handle, &kiara_appconfig_dir, file);
+    }
 }
 
 fn setup_python(handle: tauri::AppHandle) -> io::Result<HashMap<String, String>> {
-    // TODO SOME BETTER ERROR HANDLING HERE
-
-    let pixi_lock_resource = handle
-        .path_resolver()
-        .resolve_resource("resources/pixi.lock")
-        .expect("failed to find pixi.lock file");
-    let pixi_toml_resource = handle
-        .path_resolver()
-        .resolve_resource("resources/pixi.toml")
-        .expect("failed to find pixi.toml file");
-    let python_definition_resource = handle
-        .path_resolver()
-        // TODO make this be less hardcoded
-        .resolve_resource("resources/3.11.5")
-        .expect("failed to find python definition");
-    let python_deps_resource = handle
-        .path_resolver()
-        .resolve_resource("resources/requirements.txt")
-        .expect("failed to find python package dependencies");
-    let pixi_lock_hash = hash_file_at_path(&pixi_lock_resource).unwrap();
+    // define the config directory, we'll copy/install everything into $HOME/.kiara-app
     let mut kiara_appconfig_dir = dirs::home_dir().expect("Failed to get home directory");
     kiara_appconfig_dir.push(".kiara-app");
-    // TODO this isn't useful anymore now pixi is just the c compiler
-    let existing_hash_path = kiara_appconfig_dir.join(".env_hash");
-    let existing_hash = std::fs::read_to_string(&existing_hash_path);
 
-    // either no python or not up to date
-    // if existing_hash.ok().as_ref() != Some(&pixi_lock_hash) {
-        // create the config directory if not exists
-        std::fs::create_dir_all(&kiara_appconfig_dir).unwrap();
-        // Copy in the pixi files
-        std::fs::copy(pixi_lock_resource, kiara_appconfig_dir.join("pixi.lock")).unwrap();
-        std::fs::copy(pixi_toml_resource, kiara_appconfig_dir.join("pixi.toml")).unwrap();
-        std::fs::copy(python_definition_resource, kiara_appconfig_dir.join("3.11.5")).unwrap();
-        std::fs::copy(python_deps_resource, kiara_appconfig_dir.join("requirements.txt")).unwrap();
-
-        // delete .pixi directory from there if it exists
-        // let _ because I explicitly don't care if it fails (better error handling later)
-        let _ = std::fs::remove_dir_all(kiara_appconfig_dir.join(".pixi"));
-        let _ = std::fs::remove_dir_all(kiara_appconfig_dir.join("python"));
-
-        let install_result = std::process::Command::new("pixi")
-            .arg("install")
-            .current_dir(&kiara_appconfig_dir)
-            .spawn()
-            .expect("Failed to pixi install. Do you have pixi on the system.unwrap()")
-            .wait()
-            .expect("Something went wrong with pixi.unwrap()");
-        assert!(
-            install_result.success(),
-            "Failed to set up environment with pixi"
+    // is the existing python version the same?
+    log_to_frontend(&handle, "Checking for existing python...");
+    let python_exists = right_python_exists(&kiara_appconfig_dir);
+    if python_exists {
+        log_to_frontend(&handle, "Correct Python already exists");
+    } else {
+        log_to_frontend(
+            &handle,
+            "Correct version of Python doesn't exist, installing... This might take a couple of minutes",
         );
-// TODO die if this fails
-        std::fs::write(existing_hash_path, pixi_lock_hash).unwrap();
+        // clean up any old files
+        let _ = std::fs::remove_dir_all(&kiara_appconfig_dir);
+        copy_resources(&handle, &kiara_appconfig_dir);
+        let _ = compile_python(&kiara_appconfig_dir);
+        log_to_frontend(&handle, "Python installed! Installing packages...");
+        pip_install(&kiara_appconfig_dir);
+    }
 
-        let python_result = std::process::Command::new("pixi")
-            .arg("run")
-            .arg("compile-python")
-            .current_dir(&kiara_appconfig_dir)
-            .spawn()
-            .expect("Failed to run pixi command. Do you have pixi on the system.unwrap()")
-            .wait()
-            .expect("Something went wrong with pixi.unwrap()");
-        assert!(
-            install_result.success(),
-            "Failed to compile python"
-        ); // TODO die if this fails
-        let python_deps_result = std::process::Command::new("./python/bin/python")
-            .args(["-m", "pip", "install", "-r", "requirements.txt" ])
-            .current_dir(&kiara_appconfig_dir)
-            .spawn()
-            .expect("Failed to run pixi command. Do you have pixi on the system.unwrap()")
-            .wait()
-            .expect("Something went wrong with pixi.unwrap()");
-        assert!(
-            install_result.success(),
-            "Failed to install kiara"
-        ); // TODO die if this fails
-    // }
-    // else your env is already good, nothing to do
+    log_to_frontend(&handle, "Checking packages are up to date...");
+    let packages_up_to_date = right_requirements_exist(&handle, &kiara_appconfig_dir);
+    if packages_up_to_date {
+            log_to_frontend(&handle, "Packages are up to date");
+    }
+    else {
+        log_to_frontend(&handle, "Updating packages...");
+        copy_resources(&handle, &kiara_appconfig_dir);
+        pip_install(&kiara_appconfig_dir);
+        log_to_frontend(&handle, "Packages are up to date");
 
+    }
+
+    log_to_frontend(&handle, "Starting network analysis app");
     let created_env_vars = [
         (
             "DYLD_LIBRARY_PATH".to_owned(),
@@ -137,11 +180,20 @@ fn setup_python(handle: tauri::AppHandle) -> io::Result<HashMap<String, String>>
     Ok(HashMap::from(created_env_vars))
 }
 
+#[derive(Clone, serde::Serialize)]
+struct Message {
+    message: String,
+}
+
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
-            let env_vars = setup_python(app.handle()).unwrap();
-            start_main_app(env_vars);
+            let handle = app.handle();
+            std::thread::spawn(move || {
+                std::thread::sleep(Duration::from_secs(3));
+                let env_vars = setup_python(handle).unwrap();
+                start_main_app(env_vars);
+            });
             Ok(())
         })
         .run(tauri::generate_context!())
--- src/main.js
+++ src/main.js
@@ -1,18 +1,11 @@
-const { invoke } = window.__TAURI__.tauri;
+const {listen} = window.__TAURI__.event;
 
-let greetInputEl;
-let greetMsgEl;
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
+listen('logevent', (event) => {
+    console.log(event)
+    const list = document.querySelector("#steps")
+    const newStep = document.createElement('li');
+    newStep.appendChild(document.createTextNode(event.payload.message));
+    list.appendChild(newStep);
+})
